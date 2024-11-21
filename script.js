0630e2df8415021540649dfcc251a7218cbdc020
Amay Saxena <amaysaxena8@gmail.com>
2024-11-21 21:39:48 +0530

    Webpage Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function redirectToSite() {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSco39DPJK4gIxwjRDzdz-wQpb-0-6W-2DixclFdkXm5JKpvag/viewform";
+}
+function redirectToSitee() {
+    window.location.href = "https://drive.google.com/file/d/14Ht3l6CPFDbfu5Tz5KlxOGPwpf5RYt3E/view?usp=sharing";
+}
+
+function redirectToSiteee() {
+    window.location.href = " ";
+}
